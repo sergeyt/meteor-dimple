6050ea469c48dec8137fe809df3f797c86477f53
sergeyt <stodyshev@gmail.com>
2014-11-29 23:10:00 +0600

    updated to meteor@1.0 and fixed #3

--- dimple.js
+++ dimple.js
@@ -6,31 +6,28 @@
 (function (context, dimple) {
     "use strict";
 
-    if (typeof exports === "object") {
-        // CommonJS
-        module.exports = dimple(require('d3'));
-    } else {
-        if (typeof define === "function" && define.amd) {
-            // RequireJS | AMD
-            define(["d3"], function (d3) {
-                // publish dimple to the global namespace for backwards compatibility
-                // and define it as an AMD module
-                context.dimple = dimple(d3);
-                return context.dimple;
-            });
-        } else {
-            // No AMD, expect d3 to exist in the current context and publish
-            // dimple to the global namespace
-            if (!context.d3) {
-                if (console && console.warn) {
-                    console.warn("dimple requires d3 to run.  Are you missing a reference to the d3 library?");
-                } else {
-                    throw "dimple requires d3 to run.  Are you missing a reference to the d3 library?";
-                }
-            } else {
-                context.dimple = dimple(context.d3);
-            }
+    function resolve_d3() {
+      if (context.d3) {
+        return context.d3;
+      }
+      if (typeof Package !== "undefined") {
+        var d3js = Package['d3js:d3'];
+        if (d3js) {
+          return d3js.d3;
         }
+      }
+      return null;
+    }
+
+    var d3 = resolve_d3();
+    if (!d3) {
+        if (console && console.warn) {
+            console.warn("dimple requires d3 to run.  Are you missing a reference to the d3 library?");
+        } else {
+            throw "dimple requires d3 to run.  Are you missing a reference to the d3 library?";
+        }
+    } else {
+        context.dimple = dimple(d3);
     }
 
 }(this, function (d3) {
@@ -669,7 +666,7 @@
         // Copyright: 2014 PMSI-AlignAlytics
         // License: "https://github.com/PMSI-AlignAlytics/dimple/blob/master/MIT-LICENSE.txt"
         // Source: /src/objects/chart/methods/_axisIndex.js
-        // Return the ordinal value of the passed axis.  If an orientation is passed, return the order for the 
+        // Return the ordinal value of the passed axis.  If an orientation is passed, return the order for the
         // specific orientation, otherwise return the order from all axes.  Returns -1 if the passed axis isn't part of the collection
         this._axisIndex = function (axis, orientation) {
 
@@ -2620,7 +2617,7 @@
                     xIndex += 1;
                 } else if (axis.position === "y" && !this.y.hidden) {
                     if (this._deepMatch(axis)) {
-                        // Set the x co-ordinate for the y axis 
+                        // Set the x co-ordinate for the y axis
                         if (yIndex === 0) {
                             coord.x = firstOrig.x;
                         } else if (yIndex === 1) {
--- package.js
+++ package.js
@@ -2,12 +2,12 @@ Package.describe({
   name: "sergeyt:dimple",
   summary: "dimple - an object-oriented API for business analytics powered by d3.",
   git: "https://github.com/sergeyt/meteor-dimple.git",
-  version: "2.1.0"
+  version: "2.1.0_1"
 });
 
 Package.onUse(function(api){
   var client = ["client"];
-  api.versionsFrom("METEOR@0.9.1");
+  api.versionsFrom("METEOR@1.0");
+  api.use(['d3js:d3'], 'client');
   api.addFiles("dimple.js", client);
 });
-
